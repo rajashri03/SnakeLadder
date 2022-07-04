f9046c4426636faf823700324c118a3e7119304a
rajashri15 <rajashristelvelar315@gmail.com>
2022-07-04 13:12:41 +0530

    [Rajashri]Added-UC1-Start player position 0

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake Ladder!");
+
+            int playerPosition = 0;
+            Console.WriteLine(playerPosition);
+        }
+    }
+}
